387228f21f676dd2bd312e02f093e6401a5b16f3
RC908 <138607520+RC908@users.noreply.github.com>
2024-03-24 08:59:15 -0400

    Add files via upload
    New Files

---
+++ quiz_game_login.js
@@ -0,0 +1,10 @@
+
+function addUser()
+{
+    player1Name = document.getElementById("player1_name").value;
+    player2Name = document.getElementById("player2_name").value;
+
+    localStorage.setItem("P1Name", player1Name);
+    localStorage.setItem("P2Name", player2Name);
+    window.location("quiz_game_page.html");
+}
